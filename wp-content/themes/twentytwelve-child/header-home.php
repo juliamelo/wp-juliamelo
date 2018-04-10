e78c4fb2d8e2b827c9b4742088e033d6be522026
juliamelo <melojs@gmail.com>
2018-04-10 17:46:28 -0400

    added analytics tracking code to header-home and header-about

--- wp-content/themes/twentytwelve-child/header-home.php
+++ wp-content/themes/twentytwelve-child/header-home.php
@@ -29,6 +29,18 @@
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 <?php wp_head(); ?>
+
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-16332327-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-16332327-2');
+</script>
+
 </head>
 
 <body <?php body_class(); ?>>
